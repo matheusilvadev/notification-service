11cf325745bdb48232b0399b486365b449daa3bb
devma <eurotheus@gmail.com>
2026-08-17 04:40:09 -0300

    feat: enhance consumer with message logging
    - Updated NotificationConsumer to log consumed messages from RabbitMQ
    - Added log entry displaying notification ID upon message reception
    - Ensures better traceability before delegating processing to NotificationService

--- src/main/java/com/ms/notification_service/consumer/NotificationConsumer.java
+++ src/main/java/com/ms/notification_service/consumer/NotificationConsumer.java
@@ -4,9 +4,11 @@ package com.ms.notification_service.consumer;
 import com.ms.notification_service.dto.NotificationRequestDTO;
 import com.ms.notification_service.service.NotificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NotificationConsumer {
@@ -14,6 +16,7 @@ public class NotificationConsumer {
 
     @RabbitListener(queues = "${notification.rabbitmq.queue}")
     public void consumeNotificationMessage(NotificationRequestDTO dto) {
+        log.info("Message consumed from the RabbitMQ queue: ID {}", dto.id());
         service.processNotification(dto);
     }
 }
